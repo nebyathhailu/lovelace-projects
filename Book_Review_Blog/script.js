8cf696b3890f67451c28de37e18a005e8cfe949c
NHA <nebyathhailu@gmail.com>
2025-04-26 09:25:25 +0300

    Modified style

---
+++ Book_Review_Blog/script.js
@@ -0,0 +1,5 @@
+const text = document.getElementById('trevor-noah');
+
+text.onmouseover = function(){
+    text.innerHTML = "Life is full of pain. Let the pain sharpen you hold on to it. Don't be bitter."
+}
